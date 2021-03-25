b1b3779d9db6676e27ef129def1ba40b1ba54237
MerveALKAN <17701037@mersin.edu.tr>
2021-03-26 02:17:40 +0300

    .

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace InterfaceAbstractDemo
         static void Main(string[] args)
         {
             BaseCustomerManager customerManager = new NeroCustomermanager();
-            customerManager.Save(new Customer { DateOfBirth = new System.DateTime(1998, 6, 7), FirstName = "Merve", LasttName = "ALKAN", NationalityId = "52753464950" });
+            customerManager.Save(new Customer { DateOfBirth = new System.DateTime(1998, 6, 7), FirstName = "Merve", LasttName = "ALKAN", NationalityId = "52753464955" });
          
         }
     }
